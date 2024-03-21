8ad4429f3d4254b53c379c1b9d5d70b319bf858f
datdudu <c.eduardocarvalhoc@gmail.com>
2024-03-20 22:44:47 -0300

    Adding the base structure and doing tests with h2 Database

---
+++ src/main/java/com/datdudu/todolist/model/Task.java
@@ -0,0 +1,38 @@
+package com.datdudu.todolist.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tasks")
+@Setter
+@Getter
+@ToString
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date deadline;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
