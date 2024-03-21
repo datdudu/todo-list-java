c4d835d54ebcbad19cda621bc84a5d759534064e
datdudu <c.eduardocarvalhoc@gmail.com>
2024-03-21 19:02:17 -0300

    Adding README and instructions

--- src/main/java/com/datdudu/todolist/ToDoListApplication.java
+++ src/main/java/com/datdudu/todolist/ToDoListApplication.java
@@ -11,7 +11,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Todo-List Java - Nuven",
-				version = "1.0.0"
+				version = "1.0.0",
+				description = "Todo-List Simples para o teste técnico, vaga backend developer no laboratório Nuven, utilizando Spring Boot, Lombock,H2 e JPA"
 		)
 )
 public class ToDoListApplication {
